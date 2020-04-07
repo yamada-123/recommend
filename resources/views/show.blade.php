033998cb5aab23809aae502a4970cbf090bf043d
satoshi yamada <yasain0852@yahoo.co.jp>
2020-04-07 21:13:30 +0900

    apiキー作成

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,6 +6,16 @@
             <p>{{ $shop->category->name }}</p>
             <p>{{ $shop->address }}</p>
         </div>
+        
+    <iframe id='map' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyCAVZYDKAyHiIa2Q5SIgeOZbpK21bQRN1Y&amp;q={{ $shop->address }}'
+    width='100%'
+    height='320'
+    frameborder='0'>
+    </iframe>
+
+
+
+
         <div>
             <a href = {{ route('shop.list') }}>一覧に戻る</a>
                 <a href={{ route('shop.edit',['id' => $shop->id]) }}>編集</a>
